a4dd2b162fe3681fdff4b92119a5c5008feba54f
kyleghousel <69773129+kyleghousel@users.noreply.github.com>
2024-02-29 10:25:52 -0500

    Update declutter_downloads.sh
    Changed script to move files to trash instead of straight up rm/delete....less scary. Read up on what rm actually does, this is my preferred route.

--- declutter_downloads.sh
+++ declutter_downloads.sh
@@ -4,16 +4,16 @@ downloads_dir="/Users/username/Downloads"
 
 cd "$downloads_dir" || exit 1
 
-# Target filenames/types to be deleted
+# Target filenames/types to be moved to trash
 string_to_match="appname_beta"
 string2_to_match="appname_RC"
 filetype_to_match=".zip"  
 
-# Delete files matching the criteria
+# Move files to trash that match the criteria
 for file in *; do
     if [[ "$file" == "$string_to_match"* || "$file" == "$string2_to_match"* || "$file" == *"$filetype_to_match" ]]; then
-        echo "Deleting $file! Say bye-bye..."
-        rm -f "$file"
+        echo "Moving $file to trash..."
+        mv -f "$file" ~/.Trash/
     fi
 done
 
